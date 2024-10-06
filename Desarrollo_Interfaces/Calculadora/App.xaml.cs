9162e69b2800d9fd5ad16243e269a98f799fa480
F4R3 <96112394+F4R3Juan58@users.noreply.github.com>
2024-10-06 21:57:42 +0200

    Add files via upload

---
+++ Desarrollo_Interfaces/Calculadora/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.UI;
+using Microsoft.UI.Windowing;
+using Windows.Graphics;
+
+namespace Calculadora
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var windows = base.CreateWindow(activationState);
+
+            windows.Height = 660;
+            windows.Width = 400;
+            windows.MaximumHeight = 660;
+            windows.MaximumWidth = 400;
+            windows.MinimumHeight = 660;
+            windows.MinimumWidth = 400;
+
+            return windows;
+        }
+    }
+}
---
+++ Desarrollo_Interfaces/Calculadora/Calculadora.xaml.cs
@@ -0,0 +1,140 @@
+﻿using System;
+
+namespace Calculadora
+{
+    public partial class Calculadora : ContentPage
+    {
+        double memory = 0;
+        string currentExpression = string.Empty;
+
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+
+        void OnNumberClicked(object sender, EventArgs e)
+        {
+            if (sender is Button button)
+            {
+                currentExpression += button.Text;
+                display.Text = currentExpression;
+            }
+        }
+
+        void OnOperatorClicked(object sender, EventArgs e)
+        {
+            if (sender is Button button)
+            {
+                if (currentExpression.Length > 0)
+                {
+                    currentExpression += button.Text;
+                    display.Text = currentExpression;
+                }
+            }
+        }
+
+        void OnEqualClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double result = EvaluateExpression(currentExpression);
+                display.Text = result.ToString();
+                currentExpression = result.ToString();
+            }
+            catch
+            {
+                display.Text = "Error";
+                currentExpression = string.Empty;
+            }
+        }
+
+        void OnClearClicked(object sender, EventArgs e)
+        {
+            currentExpression = string.Empty;
+            display.Text = "0";
+        }
+
+        void OnMemoryAddClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double currentValue = EvaluateExpression(currentExpression);
+                memory += currentValue;
+                display.Text = "Memoria: " + memory;
+            }
+            catch
+            {
+                display.Text = "Error";
+            }
+        }
+
+        void OnMemorySubtractClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double currentValue = EvaluateExpression(currentExpression);
+                memory -= currentValue;
+                display.Text = "Memoria: " + memory;
+            }
+            catch
+            {
+                display.Text = "Error";
+            }
+        }
+
+        void OnMemoryClearClicked(object sender, EventArgs e)
+        {
+            memory = 0;
+            display.Text = "Memoria Limpiada";
+        }
+
+        double EvaluateExpression(string expression)
+        {
+            double total = 0;
+            char currentOperator = '+';
+
+            for (int i = 0; i < expression.Length; i++)
+            {
+                char ch = expression[i];
+
+                if (char.IsDigit(ch))
+                {
+                    string number = string.Empty;
+
+                    while (i < expression.Length && (char.IsDigit(expression[i]) || expression[i] == '.'))
+                    {
+                        number += expression[i];
+                        i++;
+                    }
+
+                    double value = Convert.ToDouble(number);
+
+                    switch (currentOperator)
+                    {
+                        case '+':
+                            total += value;
+                            break;
+                        case '-':
+                            total -= value;
+                            break;
+                        case '*':
+                            total *= value;
+                            break;
+                        case '/':
+                            if (value != 0)
+                                total /= value;
+                            else
+                                throw new DivideByZeroException();
+                            break;
+                    }
+                    i--;
+                }
+                else if ("+-*/".Contains(ch))
+                {
+                    currentOperator = ch;
+                }
+            }
+            return total;
+        }
+    }
+}
